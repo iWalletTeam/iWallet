94a857dc95c280107eab8f898d4229b48b346db0
Vladislav <idevnva@gmail.com>
2023-05-29 15:32:57 +0300

    Добавлена структура Icons

---
+++ iWallet/Resources/Icons.swift
@@ -0,0 +1,179 @@
+//
+//  Icons.swift
+//  iWallet
+//
+//  Created by Vladislav Novoshinskiy on 29.05.2023.
+//
+
+import Foundation
+
+struct Icons {
+    
+    // все иконки создания категории
+    static let car: String = "car"
+    static let box_truck: String = "box.truck"
+    static let car_2: String = "car.2"
+    static let creditcard: String = "creditcard"
+    static let banknote: String = "banknote"
+    static let giftcard: String = "giftcard"
+    static let person_2: String = "person.2"
+    static let person_bust: String = "person.bust"
+    static let figure_wave: String = "figure.wave"
+    static let figure_roll: String = "figure.roll"
+    static let circle_hexagonpath: String = "circle.hexagonpath"
+    static let captions_bubble: String = "captions.bubble"
+    static let network_badge_shield_half_filled: String = "network.badge.shield.half.filled"
+    static let icloud: String = "icloud"
+    static let bonjour: String = "bonjour"
+    static let figure_2_and_child_holdinghands: String = "figure.2.and.child.holdinghands"
+    static let figure_and_child_holdinghands: String = "figure.and.child.holdinghands"
+    static let face_smiling: String = "face.smiling"
+    static let house: String = "house"
+    static let sofa: String = "sofa"
+    static let fireplace: String = "fireplace"
+    static let fish: String = "fish"
+    static let hare: String = "hare"
+    static let pawprint: String = "pawprint"
+    static let popcorn: String = "popcorn"
+    static let balloon_2: String = "balloon.2"
+    static let figure_walk: String = "figure.walk"
+    static let heart: String = "heart"
+    static let pill: String = "pill"
+    static let cross: String = "cross"
+    static let fleuron: String = "fleuron"
+    static let wand_and_stars_inverse: String = "wand.and.stars.inverse"
+    static let pencil_tip: String = "pencil.tip"
+    static let wifi: String = "wifi"
+    static let antenna_radiowaves_left_and_right: String = "antenna.radiowaves.left.and.right"
+    static let network: String = "network"
+    static let book: String = "book"
+    static let graduationcap: String = "graduationcap"
+    static let pencil_and_ruler: String = "pencil.and.ruler"
+    static let backpack: String = "backpack"
+    static let tshirt: String = "tshirt"
+    static let tag: String = "tag"
+    static let gift: String = "gift"
+    static let shippingbox: String = "shippingbox"
+    static let party_popper_fill: String = "party.popper.fill"
+    static let cart: String = "cart"
+    static let carrot: String = "carrot"
+    static let airpods_chargingcase: String = "airpods.chargingcase"
+    static let airplane: String = "airplane"
+    static let sailboat: String = "sailboat"
+    static let road_lanes: String = "road.lanes"
+    static let music_mic: String = "music.mic"
+    static let theatermasks: String = "theatermasks"
+    static let birthday_cake: String = "birthday.cake"
+    static let display: String = "display"
+    static let applewatch_watchface: String = "applewatch.watchface"
+    static let gamecontroller: String = "gamecontroller"
+    static let bus_fill: String = "bus.fill"
+    static let cablecar: String = "cablecar"
+    static let steeringwheel: String = "steeringwheel"
+    static let key: String = "key"
+    static let door_right_hand_closed: String = "door.right.hand.closed"
+    static let house_and_flag: String = "house.and.flag"
+    static let arrow_triangle_2_circlepath: String = "arrow.triangle.2.circlepath"
+    static let dollarsign_arrow_circlepath: String = "dollarsign.arrow.circlepath"
+    static let hourglass: String = "hourglass"
+    static let chart_xyaxis_line: String = "chart.xyaxis.line"
+    static let creditcard_and_123: String = "creditcard.and.123"
+    static let chart_bar: String = "chart.bar"
+    static let dollarsign: String = "dollarsign"
+    static let rublesign: String = "rublesign"
+    static let eurosign: String = "eurosign"
+    static let shippingbox_circle: String = "shippingbox.circle"
+    static let timelapse: String = "timelapse"
+    static let camera_metering_matrix: String = "camera.metering.matrix"
+    static let person_fill_checkmark: String = "person.fill.checkmark"
+    static let person_crop_square_filled_and_at_rectangle_fill: String = "person.crop.square.filled.and.at.rectangle.fill"
+    static let hand_thumbsup: String = "hand.thumbsup"
+    static let percent: String = "percent"
+    static let sum: String = "sum"
+    static let number: String = "number"
+    
+    static let allIcons: [String] =
+    [
+        car,
+        box_truck,
+        car_2,
+        creditcard,
+        banknote,
+        giftcard,
+        person_2,
+        person_bust,
+        figure_wave,
+        figure_roll,
+        circle_hexagonpath,
+        captions_bubble,
+        network_badge_shield_half_filled,
+        icloud,
+        bonjour,
+        figure_2_and_child_holdinghands,
+        figure_and_child_holdinghands,
+        face_smiling,
+        house,
+        sofa,
+        fireplace,
+        fish,
+        hare,
+        pawprint,
+        popcorn,
+        balloon_2,
+        figure_walk,
+        heart,
+        pill,
+        cross,
+        fleuron,
+        wand_and_stars_inverse,
+        pencil_tip,
+        wifi,
+        antenna_radiowaves_left_and_right,
+        network,
+        book,
+        graduationcap,
+        pencil_and_ruler,
+        backpack,
+        tshirt,
+        tag,
+        gift,
+        shippingbox,
+        party_popper_fill,
+        cart,
+        carrot,
+        airpods_chargingcase,
+        airplane,
+        sailboat,
+        road_lanes,
+        music_mic,
+        theatermasks,
+        birthday_cake,
+        display,
+        applewatch_watchface,
+        gamecontroller,
+        bus_fill,
+        cablecar,
+        steeringwheel,
+        key,
+        door_right_hand_closed,
+        house_and_flag,
+        arrow_triangle_2_circlepath,
+        dollarsign_arrow_circlepath,
+        hourglass,
+        chart_xyaxis_line,
+        creditcard_and_123,
+        chart_bar,
+        dollarsign,
+        rublesign,
+        eurosign,
+        shippingbox_circle,
+        timelapse,
+        camera_metering_matrix,
+        person_fill_checkmark,
+        person_crop_square_filled_and_at_rectangle_fill,
+        hand_thumbsup,
+        percent,
+        sum,
+        number
+    ]
+}
--- iWallet/View/Templates/IconPicker.swift
+++ iWallet/View/Templates/IconPicker.swift
@@ -4,20 +4,21 @@ import SwiftUI
 
 struct IconPicker: View {
     @Binding var selectedImage: String
-    private let images: [String] =
-    [
-        "car", "box.truck", "car.2", "creditcard", "banknote", "giftcard", "person.2", "person.bust", "figure.wave", "figure.roll", "circle.hexagonpath", "captions.bubble", "network.badge.shield.half.filled", "icloud", "bonjour", "figure.2.and.child.holdinghands", "figure.and.child.holdinghands", "face.smiling", "house", "sofa", "fireplace", "fish", "hare", "pawprint", "popcorn", "balloon.2", "figure.walk", "heart", "pill", "cross", "fleuron", "wand.and.stars.inverse", "pencil.tip", "wifi", "antenna.radiowaves.left.and.right", "network", "book", "graduationcap", "pencil.and.ruler", "backpack", "tshirt", "tag", "gift", "shippingbox", "party.popper.fill", "cart", "carrot", "airpods.chargingcase", "airplane", "sailboat", "road.lanes", "music.mic", "theatermasks", "birthday.cake", "display", "applewatch.watchface", "gamecontroller", "bus.fill", "cablecar", "steeringwheel", "key", "door.right.hand.closed", "house.and.flag", "arrow.triangle.2.circlepath", "dollarsign.arrow.circlepath", "hourglass", "chart.xyaxis.line", "creditcard.and.123", "chart.bar", "dollarsign", "rublesign", "eurosign", "shippingbox.circle", "timelapse", "camera.metering.matrix", "person.fill.checkmark", "person.crop.square.filled.and.at.rectangle.fill", "hand.thumbsup", "percent", "sum", "number"
-    ]
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center) {
-                ForEach(images, id: \.self) { image in
+                ForEach(Icons.allIcons, id: \.self) { image in
                     HStack {
                         VStack {
                             Image(systemName: image)
-                                .foregroundColor(Color("colorBalanceText"))
-                                .font(.system(size: 30))
+                                .foregroundColor(Color(Colors.mainText))
+                                .font(.system(size: 20))
+                                .frame(width: 40, height: 40)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 10, style: .circular)
+                                        .strokeBorder(Color(Colors.mainText))
+                                }
                         }
                     }
                     .opacity(image == selectedImage ? 1.0 : 0.5)
@@ -26,7 +27,8 @@ struct IconPicker: View {
                         selectedImage = image
                     }
                 }
-            } .padding(.vertical, 5)
+            }
+            .padding(2)
         }
     }
 }
